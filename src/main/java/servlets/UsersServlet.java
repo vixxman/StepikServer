8bdf5eb7527ab67a4de3f5e90fb627a816e501a0
vixxman <vixxman@yandex.ru>
2019-07-19 00:25:39 +0600

    T.2f

--- src/main/java/servlets/UsersServlet.java
+++ src/main/java/servlets/UsersServlet.java
@@ -15,24 +15,4 @@ public class UsersServlet extends HttpServlet{
         this.accountService = accountService;
     }
 
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
-    }
-
-    @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPut(req, resp);
-    }
-
-    @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doDelete(req, resp);
-    }
 }
